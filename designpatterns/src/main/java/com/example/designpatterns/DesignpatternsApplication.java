1b65d52f3fb8bbf2ba6a69456ba3429dba9b5cf9
KimBeomJin <sdpthf@naver.com>
2022-04-07 23:24:21 +0900

    ⚙️ :: Init Project

---
+++ designpatterns/src/main/java/com/example/designpatterns/DesignpatternsApplication.java
@@ -0,0 +1,13 @@
+package com.example.designpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignpatternsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignpatternsApplication.class, args);
+    }
+
+}
