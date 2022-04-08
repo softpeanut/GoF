bc363a42c59579a94efd9b96ba2586aec8e23299
KimBeomJin <sdpthf@naver.com>
2022-04-08 23:42:01 +0900

    🌱 :: (#2) Thread-Safe Singleton - static inner class

--- designpatterns/src/main/java/com/example/designpatterns/singleton/Settings.java
+++ designpatterns/src/main/java/com/example/designpatterns/singleton/Settings.java
@@ -2,18 +2,13 @@ package com.example.designpatterns.singleton;
 
 public class Settings {
 
-    private static volatile Settings instance;
-
     private Settings() {}
 
+    private static class SettingsHolder {
+        private static final Settings INSTANCE = new Settings();
+    }
+
     public static Settings getInstance() {
-        if (instance == null) {
-            synchronized (Settings.class) {
-                if (instance == null) {
-                    instance = new Settings();
-                }
-            }
-        }
-        return instance;
+        return SettingsHolder.INSTANCE;
     }
 }
