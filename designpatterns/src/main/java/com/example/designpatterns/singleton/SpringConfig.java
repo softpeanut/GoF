3b343c2dc589a4e15faf5ad8a2c9d3ecd96d58dc
KimBeomJin <sdpthf@naver.com>
2022-04-09 00:26:22 +0900

    🌱 :: (#5) In Spring Singleton - singleton scope

---
+++ designpatterns/src/main/java/com/example/designpatterns/singleton/SpringConfig.java
@@ -0,0 +1,13 @@
+package com.example.designpatterns.singleton;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public String hello() {
+        return "hello";
+    }
+}
