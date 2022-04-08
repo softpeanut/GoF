167ba6a419099aac2986accb716ca5031f8da25c
KimBeomJin <sdpthf@naver.com>
2022-04-08 23:53:57 +0900

    🌱 :: (#3) Break Singleton - reflection

--- designpatterns/src/main/java/com/example/designpatterns/singleton/App.java
+++ designpatterns/src/main/java/com/example/designpatterns/singleton/App.java
@@ -1,8 +1,17 @@
 package com.example.designpatterns.singleton;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         Settings settings = Settings.getInstance();
+
+        Constructor<Settings> constructor = Settings.class.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        Settings settings1 = constructor.newInstance();
+
+        System.out.println(settings == settings1);
     }
 }
