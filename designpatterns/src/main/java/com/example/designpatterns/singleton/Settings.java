262cb04e167de8abfb99aad33f2678b8ac044b79
KimBeomJin <sdpthf@naver.com>
2022-04-08 23:40:22 +0900

    🌱 :: (#2) Thread-Safe Singleton - double checked locking

--- designpatterns/src/main/java/com/example/designpatterns/singleton/Settings.java
+++ designpatterns/src/main/java/com/example/designpatterns/singleton/Settings.java
@@ -2,11 +2,18 @@ package com.example.designpatterns.singleton;
 
 public class Settings {
 
-    private static final Settings INSTANCE = new Settings();
+    private static volatile Settings instance;
 
     private Settings() {}
 
     public static Settings getInstance() {
-        return INSTANCE;
+        if (instance == null) {
+            synchronized (Settings.class) {
+                if (instance == null) {
+                    instance = new Settings();
+                }
+            }
+        }
+        return instance;
     }
 }
