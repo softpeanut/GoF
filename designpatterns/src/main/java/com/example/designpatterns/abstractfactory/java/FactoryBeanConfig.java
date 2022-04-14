8c3cfa171a189c8cecb3c97e9f189dca0514b094
KimBeomJin <sdpthf@naver.com>
2022-04-14 21:12:07 +0900

    🌱 :: (#10) Abstract Factory - spring

---
+++ designpatterns/src/main/java/com/example/designpatterns/abstractfactory/java/FactoryBeanConfig.java
@@ -0,0 +1,13 @@
+package com.example.designpatterns.abstractfactory.java;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FactoryBeanConfig {
+
+    @Bean
+    public ShipFactory shipFactory() {
+        return new ShipFactory();
+    }
+}
