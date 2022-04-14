8768df76e3cd99552cefb6ff186cbafb7c006b24
KimBeomJin <sdpthf@naver.com>
2022-04-14 23:00:31 +0900

    🌱 :: (#11) Builder - before

---
+++ designpatterns/src/main/java/com/example/designpatterns/builder/before/App.java
@@ -0,0 +1,28 @@
+package com.example.designpatterns.builder.before;
+
+import java.time.LocalDate;
+
+public class App {
+
+    public static void main(String[] args) {
+        TourPlan shortTrip = new TourPlan();
+        shortTrip.setTitle("오레곤 롱비치 여행");
+        shortTrip.setStartDate(LocalDate.of(2021, 7, 15));
+
+        TourPlan tourPlan = new TourPlan();
+        tourPlan.setTitle("칸쿤 여행");
+        tourPlan.setNights(2);
+        tourPlan.setDays(3);
+        tourPlan.setStartDate(LocalDate.of(2020, 12, 9));
+        tourPlan.setWhereToStay("리조트");
+        tourPlan.addPlan(0, "체크인 이후 짐풀기");
+        tourPlan.addPlan(0, "저녁 식사");
+        tourPlan.addPlan(1, "조식 부페에서 식사");
+        tourPlan.addPlan(1, "해변가 산책");
+        tourPlan.addPlan(1, "점심은 수영장 근처 음식점에서 먹기");
+        tourPlan.addPlan(1, "리조트 수영장에서 놀기");
+        tourPlan.addPlan(1, "저녁은 BBQ 식당에서 스테이크");
+        tourPlan.addPlan(2, "조식 부페에서 식사");
+        tourPlan.addPlan(2, "체크아웃");
+    }
+}
---
+++ designpatterns/src/main/java/com/example/designpatterns/builder/before/DetailPlan.java
@@ -0,0 +1,37 @@
+package com.example.designpatterns.builder.before;
+
+public class DetailPlan {
+
+    private int day;
+
+    private String plan;
+
+    public DetailPlan(int day, String plan) {
+        this.day = day;
+        this.plan = plan;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public String getPlan() {
+        return plan;
+    }
+
+    public void setPlan(String plan) {
+        this.plan = plan;
+    }
+
+    @Override
+    public String toString() {
+        return "DetailPlan{" +
+                "day=" + day +
+                ", plan='" + plan + '\'' +
+                '}';
+    }
+}
