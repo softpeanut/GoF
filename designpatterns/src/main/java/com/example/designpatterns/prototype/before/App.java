349135c9cd69b3ea4efbb3f8cb2b66bb59dffa05
KimBeomJin <sdpthf@naver.com>
2022-04-16 03:48:40 +0900

    🌱 :: (#12) Prototype - before

---
+++ designpatterns/src/main/java/com/example/designpatterns/prototype/before/App.java
@@ -0,0 +1,17 @@
+package com.example.designpatterns.prototype.before;
+
+public class App {
+
+    public static void main(String[] args) {
+        GithubRepository repository = new GithubRepository();
+        repository.setUser("whiteShip");
+        repository.setName("live-study");
+
+        GithubIssue githubIssue = new GithubIssue(repository);
+        githubIssue.setId(1);
+        githubIssue.setTitle("1주차 과제: JVM은 무엇이며 자바 코드는 어떻게 실행하는 것인가.");
+
+        String url = githubIssue.getUrl();
+        System.out.println(url);
+    }
+}
