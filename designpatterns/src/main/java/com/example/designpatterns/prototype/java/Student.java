834922670fd743201ea1976925374588f93f41e4
KimBeomJin <sdpthf@naver.com>
2022-04-16 14:54:24 +0900

    🌱 :: (#12) Prototype - java

---
+++ designpatterns/src/main/java/com/example/designpatterns/prototype/java/Student.java
@@ -0,0 +1,17 @@
+package com.example.designpatterns.prototype.java;
+
+public class Student {
+
+    String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
