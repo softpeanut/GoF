566d069d1a8e33aefafb07d91f071e363f18a1a2
KimBeomJin <sdpthf@naver.com>
2022-04-25 23:00:51 +0900

    🌱 :: (#17) Facade - before

---
+++ designpatterns/src/main/java/com/example/designpatterns/_02_structuralpatterns/_05_facade/before/Client.java
@@ -0,0 +1,35 @@
+package com.example.designpatterns._02_structuralpatterns._05_facade.before;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class Client {
+
+    public static void main(String[] args) {
+        String to = "beomjin@softpeanut.me";
+        String from = "softpeanut@softpeanut.me";
+        String host = "127.0.0.1";
+
+        Properties properties = System.getProperties();
+        properties.setProperty("mail.smtp.host", host);
+
+        Session session = Session.getDefaultInstance(properties);
+
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            message.setSubject("Test Mail from Java Program");
+            message.setText("message");
+
+            Transport.send(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+}
