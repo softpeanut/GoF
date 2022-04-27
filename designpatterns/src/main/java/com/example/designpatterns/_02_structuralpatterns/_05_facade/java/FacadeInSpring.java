b370ae5e19701f8f1e645bab48390f744750e996
KimBeomJin <sdpthf@naver.com>
2022-04-27 22:16:38 +0900

    🌱 :: (#17) Facade - java

---
+++ designpatterns/src/main/java/com/example/designpatterns/_02_structuralpatterns/_05_facade/java/FacadeInSpring.java
@@ -0,0 +1,13 @@
+package com.example.designpatterns._02_structuralpatterns._05_facade.java;
+
+import org.springframework.mail.MailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+public class FacadeInSpring {
+
+    public static void main(String[] args) {
+//        MailSender mailSender = new JavaMailSenderImpl();
+//
+//        PlatformTransactionManager platformTransactionManager = new JdbcTransactionManager();
+    }
+}
